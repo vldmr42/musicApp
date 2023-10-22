3f8b5154ab22da796526c1854bead0ea53fe6c07
vldmr42 <kmb14056@yandex.ru>
2023-10-22 19:32:58 +0400

    db setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+console.log(process.env.MONGO);
+@Module({
+    imports: [
+        MongooseModule.forRoot(
+            'mongodb://mongo:DxRjlM6TMDRfTjgwCqJ7@containers-us-west-87.railway.app:7113',
+        ),
+        TrackModule,
+    ],
+})
 export class AppModule {}
---
+++ server/src/track/track.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Track, TrackDocument } from './schemas/track.schema';
+import { Model } from 'mongoose';
+import { Comment, CommentDocument } from './schemas/comment.schema';
+
+@Injectable()
+export class TrackService {
+    constructor(
+        @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+        @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
+    ) {}
+    async create() {}
+    async getAll() {}
+    async getOne() {}
+    async delete() {}
+}
