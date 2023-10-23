b0b49193e3f741651d0ae455166c51b93067c2b4
vldmr42 <kmb14056@yandex.ru>
2023-10-23 22:37:55 +0400

    track create getall getone and delete

--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Track, TrackDocument } from './schemas/track.schema';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { Comment, CommentDocument } from './schemas/comment.schema';
+import { CreateTrackDto } from './dto/create-track.dto';
+import * as mongoose from 'mongoose';
 
 @Injectable()
 export class TrackService {
@@ -10,8 +12,20 @@ export class TrackService {
         @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
         @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
     ) {}
-    async create() {}
-    async getAll() {}
-    async getOne() {}
-    async delete() {}
+    async create(dto: CreateTrackDto): Promise<Track> {
+        const track = await this.trackModel.create({ ...dto, listens: 0 });
+        return track;
+    }
+    async getAll(): Promise<Track[]> {
+        const tracks = await this.trackModel.find();
+        return tracks;
+    }
+    async getOne(id: ObjectId): Promise<Track> {
+        const track = await this.trackModel.findById(id);
+        return track;
+    }
+    async delete(id: ObjectId): Promise<mongoose.Types.ObjectId> {
+        const track = await this.trackModel.findByIdAndDelete(id);
+        return track._id;
+    }
 }
