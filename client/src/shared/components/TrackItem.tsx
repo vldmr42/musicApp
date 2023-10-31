f627cf84132a26e36766935cd49fb24259873b0b
vldmr42 <kmb14056@yandex.ru>
2023-10-31 19:28:56 +0400

    client trackList page, some styles, and next image config

--- client/src/shared/components/TrackItem.tsx
+++ client/src/shared/components/TrackItem.tsx
@@ -1,6 +1,11 @@
 import { ITrack } from '@/types/track';
-import { Card } from '@mui/material';
+import { Card, Grid, IconButton } from '@mui/material';
 import React from 'react';
+
+import styles from '@/styles/TrackItem.module.scss';
+import { Delete, Pause, PlayArrow } from '@mui/icons-material';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 interface TrackItemProps {
     track: ITrack;
@@ -8,7 +13,36 @@ interface TrackItemProps {
 }
 
 const TrackItem: React.FC<TrackItemProps> = ({ track, active = false }) => {
-    return <Card>{track.name}</Card>;
+    const router = useRouter();
+
+    return (
+        <Card
+            className={styles.track}
+            onClick={() => router.push('/tracks/' + track._id)}
+        >
+            <IconButton>{active ? <Pause /> : <PlayArrow />}</IconButton>
+            <Image
+                width={70}
+                height={70}
+                src={'http://localhost:5000/' + track.picture}
+                alt="track-image"
+            />
+            <Grid
+                container
+                direction={'column'}
+                style={{ width: 200, margin: '0 20px' }}
+            >
+                <div>{track.name}</div>
+                <div style={{ fontSize: 12, color: 'gray' }}>
+                    {track.artist}
+                </div>
+            </Grid>
+            {active && <div>02:42 / 03:22</div>}
+            <IconButton style={{ marginLeft: 'auto' }}>
+                <Delete />
+            </IconButton>
+        </Card>
+    );
 };
 
 export default TrackItem;
